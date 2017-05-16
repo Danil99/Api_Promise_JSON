a07702d0f49483d6b8a43e2e1b0606a038e0a1d4
Danya Zavoloka <Danil.nktel@gmail.com>
2017-05-17 01:34:56 +0300

    connect to mongodb with help express

--- dist/server.js
+++ dist/server.js
@@ -4,9 +4,19 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _mongodb = require('mongodb');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+var db = void 0;
+
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
 
 app.use(_express2.default.static('../dist'));
 app.set('views', '../dist');
@@ -15,6 +25,63 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(3080, function () {
-  console.log("Server started");
+var artists = [{
+  id: 1,
+  name: 'Danya'
+}, {
+  id: 2,
+  name: 'Max'
+}];
+
+app.get('/artists', function (req, res) {
+  db.collection('artists').find().toArray(function (err, docs) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(docs);
+  });
+});
+
+app.post('/artist', function (req, res) {
+  var artist = {
+    name: req.body.name
+  };
+  db.collection('artists').insert(artist, function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(artist);
+  });
+});
+
+app.put('/artist/:id', function (req, res) {
+  db.collection('artists').updateOne({ _id: (0, _mongodb.ObjectID)(req.params.id) }, { name: req.body.name }, function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.sendStatus(200);
+  });
+});
+
+app.delete('/artist/:id', function (req, res) {
+  db.collection('artists').deleteOne({ _id: (0, _mongodb.ObjectID)(req.params.id) }, function (err, result) {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.sendStatus(200);
+  });
+});
+
+_mongodb.MongoClient.connect('mongodb://localhost:27017/myapi', function (err, database) {
+  if (err) {
+    return console.log(err);
+  }
+  db = database;
+  app.listen(3080, function () {
+    console.log("Server started");
+  });
 });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import { ObjectID } from 'mongodb';
 
 let app = express();
+let db;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('../dist'));
 app.set('views', '../dist');
@@ -9,6 +16,73 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-app.listen(3080, () => {
-  console.log("Server started");
+let artists = [
+  {
+    id: 1,
+    name: 'Danya'
+  },
+  {
+    id: 2,
+    name: 'Max'
+  }
+]
+
+app.get('/artists', (req, res) => {
+  db.collection('artists').find().toArray((err, docs) => {
+    if(err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(docs)
+  })
+})
+
+app.post('/artist', (req, res) => {
+  let artist = {
+    name: req.body.name
+  };
+  db.collection('artists').insert(artist, (err, result) => {
+    if(err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(artist);
+  })
+})
+
+app.put('/artist/:id', (req, res) => {
+  db.collection('artists').updateOne(
+    { _id: ObjectID(req.params.id) },
+    { name: req.body.name },
+    (err, result) => {
+      if(err) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      res.sendStatus(200);
+    }
+  )
+})
+
+app.delete('/artist/:id', (req, res) => {
+  db.collection('artists').deleteOne(
+    { _id: ObjectID(req.params.id) },
+    (err, result) => {
+      if(err) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      res.sendStatus(200);
+    }
+  )
+})
+
+MongoClient.connect('mongodb://localhost:27017/myapi', (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  app.listen(3080, () => {
+    console.log("Server started");
+  })
 })
